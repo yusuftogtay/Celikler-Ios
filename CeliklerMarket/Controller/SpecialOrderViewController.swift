022380d004175b53a61061be9661878e5ef78517
Ahmet Yusuf TOĞTAY <yusuf.togtay@gmail.com>
2020-06-07 04:40:52 +0300

    Create SpecialOrderViewController.swift
    Co-Authored-By: Ufuk <ufukkt2699@hotmail.com>

---
+++ CeliklerMarket/Controller/SpecialOrderViewController.swift
@@ -0,0 +1,82 @@
+//
+//  specialOrderViewController.swift
+//  CeliklerMarket
+//
+//  Created by Ahmet Yusuf TOĞTAY on 29.05.2020.
+//  Copyright © 2020 OzguRND. All rights reserved.
+//
+
+import UIKit
+
+class specialOrderViewController: UIViewController, UITextViewDelegate {
+
+    
+    @IBOutlet weak var specialOrderTextField: UITextView!
+    var note: String = ""
+    
+    override func viewDidLoad() {
+        if #available(iOS 13.0, *) {
+            overrideUserInterfaceStyle = .light
+        } else {
+            // Fallback on earlier versions
+        }
+        super.viewDidLoad()
+        specialOrderTextField.delegate = self
+        specialOrderTextField.addDoneButton(title: "Bitti", target: self, selector: #selector(tapDone(sender:)))
+        specialDone.layer.cornerRadius = 6.0
+    }
+    @IBOutlet weak var specialDone: UIButton!
+    
+    @objc func tapDone(sender: Any) {
+        self.view.endEditing(true)
+    }
+    
+    private func textViewShouldEndEditing(_ textView: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        specialOrderTextField.text = String()
+    }
+    
+    @IBAction func SendMarket(_ sender: Any) {
+        if specialOrderTextField.text == "Bir Şeyler Yazın" || specialOrderTextField.text == "" {
+            let alert = UIAlertController(title: "Hata!", message: "Sipariş Alanı boş geçilmez", preferredStyle: .alert)
+
+            let action = UIAlertAction(title: "Tamam", style: .default, handler: nil)
+            alert.addAction(action)
+            
+            self.present(alert, animated: true, completion: nil)
+        } else {
+            note = specialOrderTextField.text
+            print(note)
+            UserDefaults.standard.set(note, forKey: "not")
+            performSegue(withIdentifier: "goSpecialPay", sender: nil)
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goSpecialPay" {
+            let destination = segue.destination as! paymentViewController
+            destination.note = note
+        }
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        //UserDefaults.standard.set("", forKey: "note")
+    }
+}
+
+extension UITextView {
+    func addDoneButton(title: String, target: Any, selector: Selector) {
+        
+        let toolBar = UIToolbar(frame: CGRect(x: 0.0,
+                                              y: 0.0,
+                                              width: UIScreen.main.bounds.size.width,
+                                              height: 44.0))//1
+        let flexible = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)//2
+        let barButton = UIBarButtonItem(title: title, style: .plain, target: target, action: selector)//3
+        toolBar.setItems([flexible, barButton], animated: false)//4
+        self.inputAccessoryView = toolBar//5
+    }
+}
