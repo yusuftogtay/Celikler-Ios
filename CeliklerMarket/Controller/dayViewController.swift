4d4feb86780f08b5faf47d0b62cb6e815d907406
Ahmet Yusuf TOĞTAY <yusuf.togtay@gmail.com>
2020-06-10 18:05:53 +0300

    Update dayViewController.swift

--- CeliklerMarket/Controller/dayViewController.swift
+++ CeliklerMarket/Controller/dayViewController.swift
@@ -8,27 +8,35 @@
 
 import UIKit
 
+protocol dayViewControllerDelegate
+{
+    func dayViewControllerResponse(day: String)
+}
+
 class dayViewController: UIViewController {
 
     var datee = Date()
     var timee = Date()
     var time: String = ""
     var dateString: String = ""
+    var delegate: dayViewControllerDelegate?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+    }
+    @IBAction func dayPicker(_ sender: Any) {
     }
     
-    @IBOutlet weak var dateTimePicker: UIDatePicker!
-    
-    @IBAction func DayButton(_ sender: Any) {
-        timee = dateTimePicker.date
+    @IBAction func dayButton(_ sender: Any) {
+        //timee = dateTimePicker.date
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm"
         let timeString = formatter.string(from: timee)
         time = timeString
-        performSegue(withIdentifier: "goBackDay", sender: nil)
+        if let firstViewController = self.navigationController?.viewControllers.first {
+            self.navigationController?.popToViewController(firstViewController, animated: true)
+        }
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "goBackDay" {
@@ -36,15 +44,4 @@ class dayViewController: UIViewController {
             destination.dayString = time
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
