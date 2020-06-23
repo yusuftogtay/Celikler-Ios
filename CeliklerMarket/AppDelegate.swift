fd428b69ca448ed8c15680470f55d97d60e7277f
Ahmet Yusuf TOĞTAY <yusuf.togtay@gmail.com>
2020-06-23 18:46:43 +0300

    Notification bug fixed

--- CeliklerMarket/AppDelegate.swift
+++ CeliklerMarket/AppDelegate.swift
@@ -11,6 +11,7 @@ import Firebase
 import FirebaseMessaging
 import UserNotifications
 import CoreData
+import SDWebImage
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
@@ -77,31 +78,77 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
     }
     
     func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any]) {
-      print(userInfo)
-    }
-
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
-                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        if let aps = userInfo["aps"] as? NSDictionary {
+        /*if let aps = userInfo["aps"] as? NSDictionary {
             let body_notifica = aps["alert"]! as! NSDictionary
             let title = body_notifica["title"]! as! String
-            let body = body_notifica["message"]! as! String
-            let image = userInfo["image"] as! String
+            let body = body_notifica["body"]! as! String
+            var image = userInfo["image"] as! String
+            image =  "https://amasyaceliklermarket.com" + image
             guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
             let context = appDelegate.persistentContainer.viewContext
             let newCard = NSEntityDescription.insertNewObject(forEntityName: "Notifications", into: context)
             newCard.setValue(title, forKeyPath: "title")
             newCard.setValue(body, forKey: "body")
             newCard.setValue(image, forKey: "image")
-        }
-        
+            do {
+                try context.save()
+            } catch {
+                print(error)
+            }
+            print("ust " + "\(userInfo)")
+        }*/
+    }
+
+    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+                     fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
         if application.applicationState == UIApplication.State.inactive || application.applicationState == UIApplication.State.background{
             let board  : UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
             let tabbar =  board.instantiateViewController(withIdentifier: "tabBarViewController") as! UITabBarController
+            tabbar.selectedIndex = 3
             window?.rootViewController = tabbar
         }
-        print(userInfo)
-
+    
+        if userInfo["title"] != nil {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            let context = appDelegate.persistentContainer.viewContext
+            let newCard = NSEntityDescription.insertNewObject(forEntityName: "Notifications", into: context)
+            var image = userInfo["image"] as! String
+            image =  "https://amasyaceliklermarket.com" + image
+            newCard.setValue(userInfo["title"], forKeyPath: "title")
+            newCard.setValue(userInfo["message"], forKey: "body")
+            newCard.setValue(image, forKey: "image")
+            do {
+                try context.save()
+            } catch {
+                print(error)
+            }
+            let content = UNMutableNotificationContent()
+            content.title = userInfo["title"] as! String
+            content.body =  userInfo["message"] as! String
+            content.sound = UNNotificationSound.default
+            
+            /*let url = URL(string: image)
+            DispatchQueue.global().async { [weak self] in
+                if let data = try? Data(contentsOf: url!) {
+                    if let image = UIImage(data: data) {
+                        DispatchQueue.main.async {
+                           let myImage = UIImage(data: data)!
+                            
+                        }
+                    }
+                }
+            }*/
+            let identifier = "FirstUserNotification"
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+            let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+            UNUserNotificationCenter.current().add(request) { (error) in
+                if error != nil {
+                    print("Something wrong")
+                }
+            }
+            
+        }
+        completionHandler(UIBackgroundFetchResult.newData)
     }
     
     func applicationDidBecomeActive(_ application: UIApplication) {
@@ -167,9 +214,65 @@ class AppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate {
 }
 
 extension AppDelegate: UNUserNotificationCenterDelegate {
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+    /*func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         print("\(#function)")
         completionHandler(.alert)
+    }*/
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+      withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+      let userInfo = notification.request.content.userInfo
+
+      // With swizzling disabled you must let Messaging know about the message, for Analytics
+      // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+      // Print message ID.
+      /*if let messageID = userInfo[gcmMessageIDKey] {
+        print("Message ID: \(messageID)")
+      }*/
+
+      // Print full message.
+      print("dddddd" + "\(userInfo)")
+
+      // Change this to your preferred presentation option
+      completionHandler([[.alert, .sound]])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+      let userInfo = response.notification.request.content.userInfo
+      // Print message ID.
+      /*if let messageID = userInfo[gcmMessageIDKey] {
+        print("Message ID: \(messageID)")
+      }*/
+
+      // Print full message.
+      print(userInfo)
+
+      completionHandler()
+    }
+}
+
+extension UNNotificationAttachment {
+
+    static func create(identifier: String, image: UIImage, options: [NSObject : AnyObject]?) -> UNNotificationAttachment? {
+        let fileManager = FileManager.default
+        let tmpSubFolderName = ProcessInfo.processInfo.globallyUniqueString
+        let tmpSubFolderURL = URL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(tmpSubFolderName, isDirectory: true)
+        do {
+            try fileManager.createDirectory(at: tmpSubFolderURL, withIntermediateDirectories: true, attributes: nil)
+            let imageFileIdentifier = identifier+".png"
+            let fileURL = tmpSubFolderURL.appendingPathComponent(imageFileIdentifier)
+            let imageData = UIImage.pngData(image)
+            try imageData()?.write(to: fileURL)
+            let imageAttachment = try UNNotificationAttachment.init(identifier: imageFileIdentifier, url: fileURL, options: options)
+            return imageAttachment
+        } catch {
+            print("error " + error.localizedDescription)
+        }
+        
+        return nil
     }
 }
 
