dc09539c40c48bd44c63118d528873a6a186bdd3
Ahmet Yusuf TOĞTAY <yusuf.togtay@gmail.com>
2020-07-22 03:23:28 +0300

    Create launchScreenCollectionViewCell.swift

---
+++ CeliklerMarket/Controller/launchScreenCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  launchScreenCollectionViewCell.swift
+//  CeliklerMarket
+//
+//  Created by Ahmet Yusuf TOĞTAY on 21.07.2020.
+//  Copyright © 2020 OzguRND. All rights reserved.
+//
+
+import UIKit
+
+class launchScreenCollectionViewCell: UICollectionViewCell {
+    
+    viewDidlo
+}
